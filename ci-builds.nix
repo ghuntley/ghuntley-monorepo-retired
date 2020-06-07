8ee2e3df0f2dfb96bcb961be4a61c5db8c1a9af3
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-07 22:52:05 +1000

    Create ci-builds.nix

---
+++ ci-builds.nix
@@ -0,0 +1,10 @@
+# This file defines the derivations that should be built by CI.
+#
+# The plan is still to implement recursive tree traversal
+# automatically and detect all derivations that have `meta.enableCI =
+# true`, but this is currently more effort than it would save me.
+
+with (import ./default.nix {}); [
+  web.ghuntley-com
+ ]
+ 
