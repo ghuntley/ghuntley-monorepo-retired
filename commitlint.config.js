032a9f6592ecc95c53698e44111cb554d26f8591
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-10 12:32:30 +0000

    a

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
+// SPDX-License-Identifier: Proprietary
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 }
+
--- web/ghuntley.com/shell.nix
+++ web/ghuntley.com/shell.nix
@@ -1,3 +1,6 @@
+# Copyright (c) 2020 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
+# SPDX-License-Identifier: Proprietary
+
 with import <nixpkgs> {};
 
 mkShell rec {
@@ -6,3 +9,4 @@ mkShell rec {
     (with dotnetCorePackages; combinePackages [ sdk_3_1 ])
   ];
 }
+
