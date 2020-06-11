7af899662253be6283af89b29b98137b24cc1134
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-11 19:38:30 +1000

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  
+
   // https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
   rules: {
     'header-max-length': [1, 'always', 72],
