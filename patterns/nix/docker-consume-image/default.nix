52f7b12620ee14716c08f27f74949bc3d0f33bc9
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-12 21:36:53 +0000

    a

--- patterns/nix/docker-consume-image/default.nix
+++ patterns/nix/docker-consume-image/default.nix
@@ -1,9 +1,2 @@
-#!/usr/bin/env python
 # Copyright (c) 2020 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
 # SPDX-License-Identifier: Proprietary
-
-
-import sys
-
-print("Hello, World!")
-
