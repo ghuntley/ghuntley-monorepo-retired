8b4afec0cc3c1afb51958d71962783ddde5ae2a9
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-13 18:48:04 +0000

    a

--- web/ghuntley.com/Program.cs
+++ web/ghuntley.com/Program.cs
@@ -7,19 +7,23 @@ using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
 
-namespace Statiqdev
+namespace StatiqWebsite
 {
     public static class Program
     {
         public static async Task<int> Main(string[] args) =>
             await Bootstrapper.Factory
                 .CreateWeb(args)
+                .DeployToNetlify(
+                    Environment.GetEnvironmentVariable("NETLIFY_SITE_ID"),
+                    Environment.GetEnvironmentVariable("NETLIFY_ACCESS_TOKEN")
+                )
+                .AddSetting(WebKeys.NetlifyRedirects, false)
                 .AddSetting(Keys.Host, "ghuntley.com")
                 .AddSetting(Keys.LinksUseHttps, true)
-                .AddSetting(WebKeys.MirrorResources, false)
+                .AddSetting(WebKeys.MirrorResources, true)
                 .AddSetting(WebKeys.GenerateSitemap, true)
                 .AddSetting("EditLink", Config.FromDocument((doc, ctx) => "https://github.com/ghuntley/ghuntley/edit/trunk/web/ghuntley.com/input/" + doc.Source.GetRelativeInputPath()))
-                .AddSetting(SiteKeys.NoChildPages, Config.FromDocument(doc => doc.Destination.Segments[0].SequenceEqual("blog".AsMemory())))
                 .AddPipelines()
                 .RunAsync();
     }
--- web/ghuntley.com/SiteKeys.cs
+++ web/ghuntley.com/SiteKeys.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Statiqdev
+namespace StatiqWebsite
 {
     public static class SiteKeys
     {
-        public const string NoContainer = nameof(NoContainer);
-        public const string NoSidebar = nameof(NoSidebar);
-        public const string Topic = nameof(Topic);
-        public const string NoChildPages = nameof(NoChildPages);
-        public const string BreadcrumbTitle = nameof(BreadcrumbTitle); // The title of a page when displayed in the breadcrumbs
     }
 }
