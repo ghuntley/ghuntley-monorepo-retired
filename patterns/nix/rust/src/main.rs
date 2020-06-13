03790d3782fb0ce516e28ff19057965ce2cbd1cd
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-06-13 09:45:03 +0000

    a

---
+++ patterns/nix/rust/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+
+
+#[test]
+fn test_main() {
+    main();
+}
