10f1684fb808b39b784881c1e1686a8ad1163fa7
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-09-12 12:28:40 +0000

    refactor(ops): relocated pulumi configuration

---
+++ ops/pulumi/ghuntley.net/Deployment.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
+// SPDX-License-Identifier: Proprietary
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+partial class Infrastructure
+{
+    public Infrastructure()
+    {
+        CloudflareDNS();
+    }
+}
+
--- ops/pulumi/ghuntley.net/Program.cs
+++ ops/pulumi/ghuntley.net/Program.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2020 Geoffrey Huntley <ghuntley@ghuntley.com>. All rights reserved.
 // SPDX-License-Identifier: Proprietary
 
-ï»¿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main() => Deployment.RunAsync<DepotStack>();
+    static Task<int> Main() => Deployment.RunAsync<Infrastructure>();
 }
 
