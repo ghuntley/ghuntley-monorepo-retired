357c7a05fee412e5394516f4b55eba5dd56326d9
Geoffrey Huntley <ghuntley@ghuntley.com>
2020-09-27 02:07:00 +0000

    infra(desktops): cfg xinput on launch

---
+++ dotfiles/linux/i3/.config/i3/xinput-cfg.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Kensington Slimblade
+xinput set-int-prop "Kensington Kensington Slimblade Trackball" "Evdev Middle Button Emulation" 8 0 7 8 9
+xinput set-button-map "Kensington Kensington Slimblade Trackball" 1 8 2 4 5 6 7 3 2
+xinput set-int-prop "Kensington Kensington Slimblade Trackball" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "Kensington Kensington Slimblade Trackball" "Evdev Wheel Emulation Button" 8 8
+xinput set-int-prop "Kensington Kensington Slimblade Trackball" "Evdev Wheel Emulation Axes" 8 6 7 4 5
+xinput set-int-prop "Kensington Kensington Slimblade Trackball" "Evdev Wheel Emulation Timeout" 16 300
+xinput set-prop "Kensington Kensington Slimblade Trackball" "libinput Accel Speed" 1
+
